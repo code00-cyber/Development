eec59f985a4b7d4b773c8ff9bc6ef682ba2bd1a6
code00-cyber <85999887+code00-cyber@users.noreply.github.com>
2023-12-12 16:43:41 +0000

    Added api route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jasonifyfy
 app=Flask(__name__)
 
 JOBS=[
@@ -35,7 +35,11 @@ JOBS=[
 @app.route("/")
 def hello_world():
   return render_template('home.html',
-                        jobs=JOBS)
+                        jobs=JOBS, company_name='Google')
 
+@app.route("/api/jobs")
+def list_jobs():
+  return jasonifyfy(JOBS)
+    
 if __name__ == "__main__":
   app.run(host="0.0.0.0", debug=True)
